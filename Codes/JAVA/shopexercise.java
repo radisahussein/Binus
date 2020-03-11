1c980cfa4e7d59b5ae59cf8134fd1a9cc203ad19
radisahussein <56021204+radisahussein@users.noreply.github.com>
2020-03-11 10:33:21 +0700

    TA Assignment - Shop Exercise

---
+++ Codes/JAVA/shopexercise.java
@@ -0,0 +1,87 @@
+// Radisa Hussein Rachmadi - 2301891752
+
+import java.util.Scanner;
+
+public class shopexercise
+{
+    public static void main(String[] arg)
+    {   
+        //Creating all the variables
+        String productName[] = {"Processor", "RAM" , "Power Supply","Graphic Card","SSD"};
+        int productPrice[] = {2560000,1250000,870000,3850000,1050000};
+        int totalPrice;
+        int selectProduct;
+        int selectAmount;
+        int inputMoney;
+
+        //Menu
+        System.out.println("Computer Shop");
+        System.out.println("===========================");
+        
+        for(int i = 0; i < productName.length;i++)
+        {
+            System.out.print((i+1) + ". " + productName[i] + " - " + "Rp." + productPrice[i] + "\n");
+        }
+        System.out.println("===========================");
+        
+        //Initializing Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //Choosing Product
+        System.out.println("Choose Product: ");
+        selectProduct = sc.nextInt();
+    
+        if(selectProduct <= productName.length & selectProduct > 0)
+        {
+            System.out.println("Selected Product : " + productName[selectProduct-1]);
+            System.out.println("===========================");
+
+            // Choosing Amount
+            System.out.println("Input Quantity [1-20]: ");
+            selectAmount = sc.nextInt();
+            if(selectAmount<= 20 & selectAmount > 0)
+            {
+                System.out.println("===========================");
+
+                //Calculating Total Price
+                totalPrice = selectAmount * productPrice[selectProduct-1];
+                System.out.println("Total Price : Rp." + totalPrice);
+                System.out.println("Input money [bigger or equals to price]: ");
+                inputMoney = sc.nextInt();
+
+                //Checking if money is enough
+                if(inputMoney == totalPrice)
+                {
+                    System.out.println("You pay with the exact amount of money!");
+                }
+                
+                else if(inputMoney > totalPrice)
+                {
+                    System.out.println("You get Rp." + (inputMoney-totalPrice) + " for the change");
+                }
+
+                else
+                {
+                    System.out.println("You don't have enough money!");
+                }
+            }
+
+            else
+            {
+                System.out.println("Invalid Quantity!");
+                System.exit(0);
+            }
+        }
+
+        else
+        {
+            System.out.println("Invalid Product!");
+            System.exit(0);
+        }
+
+        sc.close();
+    }
+
+    
+
+}
