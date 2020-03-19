b4a7a7f8ade5c30014b9816e7a06c3a53bd41aae
radisahussein <56021204+radisahussein@users.noreply.github.com>
2020-03-19 10:14:42 +0700

    triangle assignment for Mr.Andreas

---
+++ Codes/C++/SEM 2/assignment1.h
@@ -0,0 +1,43 @@
+#ifndef HEADER_HEADER_H
+#define HEADER_HEADER_H
+#include <iostream>
+using namespace std;
+
+
+int mainProgram(int rows)
+{
+    int count = 0, count1 = 0, k = 0;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int space = 1; space <= rows-i; ++space)
+        {
+            cout << "  ";
+            ++count;
+        }
+
+        while(k != 2*i-1)
+        {
+            if (count <= rows-1)
+            {
+                cout << i+k << " ";
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                cout << i+k-2*count1 << " ";
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+#endif
